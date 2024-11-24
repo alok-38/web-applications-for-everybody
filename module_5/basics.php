6e2e9ee2318f7c9af2a6f867c9df1ea523ebf4f7
Alokananda Y <plantsfortrees@gmail.com>
2024-11-24 14:38:48 +0000

    PHP - Basics

---
+++ module_5/basics.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Basics</title>
+</head>
+<body>
+    <h1>Hello from Dr. Chuck's HTML Page</h1>
+    <p>
+        <?php
+            echo "Hi there.\n";
+            $answer = 6 * 7;
+            echo "The answer is $answer, What ";
+            echo "Was the question again?\n";
+        ?>
+    </p>
+    <p>Yes. Another paragraph!</p>
+</body>
+</html>
